4ea37ddf90cd3d2fd3013c334d077329c21a8a5a
Abel-Dev19 <abelmitiku407@gmail.com>
2026-04-05 00:57:09 +0300

    Products bug fix

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -1,5 +1,6 @@
 // app/products/add/page.tsx
 import ProductForm from "./components/form"; // adjust path
+export const dynamic = "force-dynamic";
 
 export default function AddProductPage() {
   return <ProductForm mode="create" />;
